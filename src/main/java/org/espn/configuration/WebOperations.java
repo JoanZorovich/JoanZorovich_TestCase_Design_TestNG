cf119ad172c3f3d1e91834edd9cb552baf047ced
JoanZorovich <joanzorovichg@gmail.com>
2022-11-13 21:53:14 -0500

    Basic configuration

---
+++ src/main/java/org/espn/configuration/WebOperations.java
@@ -0,0 +1,46 @@
+package org.espn.configuration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.openqa.selenium.support.PageFactory.initElements;
+
+public class WebOperations {
+    //Metodos para interactuar con los elementos de la página
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    public WebOperations(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5L));
+        initElements(driver, this);
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+
+    public void clickElement(WebElement element){
+        waitForClickable(element);
+        element.click();
+    }
+
+    public void typeOnInput(WebElement element, String text){
+        element.sendKeys(text);
+    }
+
+    public void waitForVisibility(WebElement element){
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void waitForClickable(WebElement element){
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+}
+
---
+++ src/test/java/org/espn/pages/BasePage.java
@@ -0,0 +1,39 @@
+package org.espn.pages;
+
+import org.espn.configuration.WebOperations;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class BasePage extends WebOperations {
+
+    @FindBy(css = ".container h1")
+    private WebElement espnLogo;
+
+    @FindBy(id = "global-user-trigger")
+    private WebElement userButton;
+
+    public BasePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean isEspnLogoDisplayed(){
+        return espnLogo.isDisplayed();
+    }
+
+    public boolean isUserButtonDisplayed(){
+        return userButton.isDisplayed();
+    }
+
+    public HomePage actionForClickUserButton(){
+        super.waitForVisibility(espnLogo);
+        super.waitForVisibility(userButton);
+
+        if(isEspnLogoDisplayed() && isUserButtonDisplayed()){
+            super.clickElement(userButton);
+            return new HomePage(super.getDriver());
+        }
+
+        return null;
+    }
+}
