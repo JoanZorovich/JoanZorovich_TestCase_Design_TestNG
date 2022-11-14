d2bf2545f29dc52545c7a6e2590c770096ab300c
JoanZorovich <joanzorovichg@gmail.com>
2022-11-13 22:59:52 -0500

    create basePage

--- src/test/java/org/espn/pages/BasePage.java
+++ src/test/java/org/espn/pages/BasePage.java
@@ -7,33 +7,5 @@ import org.openqa.selenium.support.FindBy;
 
 public class BasePage extends WebOperations {
 
-    @FindBy(css = ".container h1")
-    private WebElement espnLogo;
 
-    @FindBy(id = "global-user-trigger")
-    private WebElement userButton;
-
-    public BasePage(WebDriver driver) {
-        super(driver);
-    }
-
-    public boolean isEspnLogoDisplayed(){
-        return espnLogo.isDisplayed();
-    }
-
-    public boolean isUserButtonDisplayed(){
-        return userButton.isDisplayed();
-    }
-
-    public HomePage actionForClickUserButton(){
-        super.waitForVisibility(espnLogo);
-        super.waitForVisibility(userButton);
-
-        if(isEspnLogoDisplayed() && isUserButtonDisplayed()){
-            super.clickElement(userButton);
-            return new HomePage(super.getDriver());
-        }
-
-        return null;
-    }
 }
